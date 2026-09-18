19b921b721328e0b471f736e5f6e663d053e6a92
Iurie Nistor <iuriehn@gmail.com>
2026-09-18 08:45:58 +0300

    bitcrasher, empty cpp process function

---
+++ src/dsp/src/ent_bitcrasher.c
@@ -0,0 +1,168 @@
+/**
+ * File name: ent_bitcrasher.c
+ * Project: Entropictron (A texture synthesizer)
+ *
+ * Copyright (C) 2025 Iurie Nistor
+ *
+ * This file is part of Entropictron.
+ *
+ * Entropictron is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "ent_bitcrasher.h"
+#include "ent_state_internal.h"
+#include "qx_math.h"
+
+#include <stdlib.h>
+
+struct ent_bitcrasher {
+        // Parameters
+        bool enabled;
+        int bits;
+        float rate;
+        float chaos;
+        float fold;
+        float mix;
+};
+
+struct ent_bitcrasher* ent_bitcrasher_create(int sample_rate)
+{
+        (void)sample_rate;
+
+        struct ent_bitcrasher *b = calloc(1, sizeof(*b));
+        if (!b)
+                return NULL;
+
+        b->bits = ENT_BITCRASHER_DEFAULT_BITS;
+        b->rate = ENT_BITCRASHER_DEFAULT_RATE;
+        b->chaos = ENT_BITCRASHER_DEFAULT_CHAOS;
+        b->fold = ENT_BITCRASHER_DEFAULT_FOLD;
+        b->mix = ENT_BITCRASHER_DEFAULT_MIX;
+
+        return b;
+}
+
+void ent_bitcrasher_free(struct ent_bitcrasher **b)
+{
+        if (b && *b) {
+                free(*b);
+                *b = NULL;
+        }
+}
+
+enum ent_error ent_bitcrasher_enable(struct ent_bitcrasher *b, bool enabled)
+{
+        b->enabled = enabled;
+        return ENT_OK;
+}
+
+bool ent_bitcrasher_is_enabled(const struct ent_bitcrasher *b)
+{
+        return b->enabled;
+}
+
+enum ent_error ent_bitcrasher_set_bits(struct ent_bitcrasher *b, int bits)
+{
+        b->bits = QX_CLAMP(bits,
+                           ENT_BITCRASHER_MIN_BITS,
+                           ENT_BITCRASHER_MAX_BITS);
+        return ENT_OK;
+}
+
+int ent_bitcrasher_get_bits(const struct ent_bitcrasher *b)
+{
+        return b->bits;
+}
+
+enum ent_error ent_bitcrasher_set_rate(struct ent_bitcrasher *b, float rate)
+{
+        b->rate = qx_clamp_float(rate,
+                                 ENT_BITCRASHER_MIN_RATE,
+                                 ENT_BITCRASHER_MAX_RATE);
+        return ENT_OK;
+}
+
+float ent_bitcrasher_get_rate(const struct ent_bitcrasher *b)
+{
+        return b->rate;
+}
+
+enum ent_error ent_bitcrasher_set_chaos(struct ent_bitcrasher *b, float chaos)
+{
+        b->chaos = qx_clamp_float(chaos,
+                                  ENT_BITCRASHER_MIN_CHAOS,
+                                  ENT_BITCRASHER_MAX_CHAOS);
+        return ENT_OK;
+}
+
+float ent_bitcrasher_get_chaos(const struct ent_bitcrasher *b)
+{
+        return b->chaos;
+}
+
+enum ent_error ent_bitcrasher_set_fold(struct ent_bitcrasher *b, float fold)
+{
+        b->fold = qx_clamp_float(fold,
+                                 ENT_BITCRASHER_MIN_FOLD,
+                                 ENT_BITCRASHER_MAX_FOLD);
+        return ENT_OK;
+}
+
+float ent_bitcrasher_get_fold(const struct ent_bitcrasher *b)
+{
+        return b->fold;
+}
+
+enum ent_error ent_bitcrasher_set_mix(struct ent_bitcrasher *b, float mix)
+{
+        b->mix = qx_clamp_float(mix,
+                                ENT_BITCRASHER_MIN_MIX,
+                                ENT_BITCRASHER_MAX_MIX);
+        return ENT_OK;
+}
+
+float ent_bitcrasher_get_mix(const struct ent_bitcrasher *b)
+{
+        return b->mix;
+}
+
+void ent_bitcrasher_process(struct ent_bitcrasher *b, float **data, size_t size)
+{
+        (void)b;
+        (void)data;
+        (void)size;
+}
+
+void ent_bitcrasher_set_state(struct ent_bitcrasher *b,
+                              const struct ent_state_bitcrasher *state)
+{
+        ENT_SET_STATE(b, state, enabled, ent_bitcrasher_enable);
+        ENT_SET_STATE(b, state, bits, ent_bitcrasher_set_bits);
+        ENT_SET_STATE(b, state, rate, ent_bitcrasher_set_rate);
+        ENT_SET_STATE(b, state, chaos, ent_bitcrasher_set_chaos);
+        ENT_SET_STATE(b, state, fold, ent_bitcrasher_set_fold);
+        ENT_SET_STATE(b, state, mix, ent_bitcrasher_set_mix);
+}
+
+void ent_bitcrasher_get_state(const struct ent_bitcrasher *b,
+                              struct ent_state_bitcrasher *state)
+{
+        ENT_GET_STATE(b, state, enabled, ent_bitcrasher_is_enabled);
+        ENT_GET_STATE(b, state, bits, ent_bitcrasher_get_bits);
+        ENT_GET_STATE(b, state, rate, ent_bitcrasher_get_rate);
+        ENT_GET_STATE(b, state, chaos, ent_bitcrasher_get_chaos);
+        ENT_GET_STATE(b, state, fold, ent_bitcrasher_get_fold);
+        ENT_GET_STATE(b, state, mix, ent_bitcrasher_get_mix);
+}
---
+++ src/dsp/src/ent_bitcrasher.h
@@ -0,0 +1,98 @@
+/**
+ * File name: ent_bitcrasher.h
+ * Project: Entropictron (A texture synthesizer)
+ *
+ * Copyright (C) 2025 Iurie Nistor
+ *
+ * This file is part of Entropictron.
+ *
+ * Entropictron is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef ENT_BITCRASHER_H
+#define ENT_BITCRASHER_H
+
+#include "ent_defs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ENT_BITCRASHER_MIN_BITS 1
+#define ENT_BITCRASHER_MAX_BITS 16
+#define ENT_BITCRASHER_DEFAULT_BITS 8
+
+#define ENT_BITCRASHER_MIN_RATE 0.01f
+#define ENT_BITCRASHER_MAX_RATE 1.0f
+#define ENT_BITCRASHER_DEFAULT_RATE 1.0f
+
+#define ENT_BITCRASHER_MIN_CHAOS 0.0f
+#define ENT_BITCRASHER_MAX_CHAOS 1.0f
+#define ENT_BITCRASHER_DEFAULT_CHAOS 0.0f
+
+#define ENT_BITCRASHER_MIN_FOLD 0.0f
+#define ENT_BITCRASHER_MAX_FOLD 1.0f
+#define ENT_BITCRASHER_DEFAULT_FOLD 0.0f
+
+#define ENT_BITCRASHER_MIN_MIX 0.0f
+#define ENT_BITCRASHER_MAX_MIX 1.0f
+#define ENT_BITCRASHER_DEFAULT_MIX 1.0f
+
+struct ent_bitcrasher;
+struct ent_state_bitcrasher;
+
+struct ent_bitcrasher* ent_bitcrasher_create(int sample_rate);
+
+void ent_bitcrasher_free(struct ent_bitcrasher **b);
+
+enum ent_error ent_bitcrasher_enable(struct ent_bitcrasher *b, bool enabled);
+
+bool ent_bitcrasher_is_enabled(const struct ent_bitcrasher *b);
+
+enum ent_error ent_bitcrasher_set_bits(struct ent_bitcrasher *b, int bits);
+
+int ent_bitcrasher_get_bits(const struct ent_bitcrasher *b);
+
+enum ent_error ent_bitcrasher_set_rate(struct ent_bitcrasher *b, float rate);
+
+float ent_bitcrasher_get_rate(const struct ent_bitcrasher *b);
+
+enum ent_error ent_bitcrasher_set_chaos(struct ent_bitcrasher *b, float chaos);
+
+float ent_bitcrasher_get_chaos(const struct ent_bitcrasher *b);
+
+enum ent_error ent_bitcrasher_set_fold(struct ent_bitcrasher *b, float fold);
+
+float ent_bitcrasher_get_fold(const struct ent_bitcrasher *b);
+
+enum ent_error ent_bitcrasher_set_mix(struct ent_bitcrasher *b, float mix);
+
+float ent_bitcrasher_get_mix(const struct ent_bitcrasher *b);
+
+void ent_bitcrasher_process(struct ent_bitcrasher *b,
+                            float **data,
+                            size_t size);
+
+void ent_bitcrasher_set_state(struct ent_bitcrasher *b,
+                              const struct ent_state_bitcrasher *state);
+
+ void ent_bitcrasher_get_state(const struct ent_bitcrasher *b,
+                               struct ent_state_bitcrasher *state);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ENT_BITCRASHER_H
